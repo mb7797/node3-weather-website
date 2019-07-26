c6eaf4072a71aeba44d985d050a175ec2dface74
mb7797 <bierlich.m@gmail.com>
2019-07-26 18:26:09 +0200

    UV-Index added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,9 @@
 const request = require('request')
 
+
+
+
+
 const forecast = (number1, number2, callback)=>{
   const url = 'https://api.darksky.net/forecast/8875d0ccc2fa21bd8950a5a4d0c1c574/'+ number1 + ',' + number2 + '?units=si&lang=de'
   request({url, json: true}, (error, { body })=>{
@@ -8,7 +12,7 @@ const forecast = (number1, number2, callback)=>{
     }else if(body.error){
       callback('Unable to finde location', undefined)
     }else {
-      callback(undefined, body.daily.data[0].summary + '\n Die Temperatur liegt bei : ' + body.currently.temperature + ' Grad. Es herrscht eine Regenwahrscheinlichkeit von: ' + body.currently.precipProbability + ' Prozent')
+      callback(undefined, body.daily.data[0].summary + '\n Die Temperatur liegt bei : ' + body.currently.temperature + ' Grad. Es herrscht eine Regenwahrscheinlichkeit von: ' + body.currently.precipProbability + ' Prozent. Der UV-Index liegt bei: ' + body.currently.uvIndex + '.')
     }
   })
 }
